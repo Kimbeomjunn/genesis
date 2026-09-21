b2acea2922d04b51eb44e37dd4b14f1bd0f64dc9
beomjunkim <akbj0213@gmail.com>
2026-09-21 20:02:36 +0900

    Docker Compose로 React+SpringBoot+MySQL 통합 실행 환경 구성

--- genesis_be/src/main/java/com/example/genesis_be/HealthController.java
+++ genesis_be/src/main/java/com/example/genesis_be/HealthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:8081"})
 public class HealthController {
 
     @GetMapping("/api/health")
